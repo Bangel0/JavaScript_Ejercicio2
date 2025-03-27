944e83fe216c93bb3e61ad8a5e5bdc6e34165792
Brahian <angelbrahian026@gmail.com>
2025-03-26 20:03:38 -0500

    Creacion de variables para el funcionamiento del programa

---
+++ Script.js
@@ -0,0 +1,20 @@
+/*Creación de variables para el funcionamiento del programa*/
+
+let continuar = true
+
+let vendedorActual = ''
+let totalVendidos = 0
+let comisiónTotal = 0
+
+// Variables para almacenar algunas
+let maxVentasVendedor = ''
+let maxVentasMonto = 0
+let totalElectrodomestícos = 0
+let totalTecnología = 0
+let totalHogar = 0
+
+let numVendedores = 0
+
+console.log(
+  'Sistema de gestión de venta - Tienda de hogar, Electrodoméstico, Tecnología'
+)
